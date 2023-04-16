4df0156fc243c2c354890ea7ceca2a8a8eb97c62
Aidan Boland <aidanboland1@icloud.com>
2023-04-16 22:24:59 +0100

    initial config

---
+++ lua/aidanboland/init.lua
@@ -0,0 +1,9 @@
+require("aidanboland.remap")
+require("aidanboland.set")
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = buffer,
+        callback = function()
+            vim.lsp.buf.format { async = false }
+        end
+    })
