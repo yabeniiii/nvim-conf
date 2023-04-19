ac23f668eb40113ee206c42b7defa0f21d46d26d
Aidan Boland <aidanboland1@icloud.com>
2023-04-19 19:01:53 +0100

    update

--- lua/aidanboland/packer.lua
+++ lua/aidanboland/packer.lua
@@ -11,13 +11,13 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
 
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use { "kabouzeid/nvim-jellybeans", requires = "rktjmp/lush.nvim", config = function()
         vim.cmd('colorscheme jellybeans')
     end }
-    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+    use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
     use('theprimeagen/harpoon')
     use('tpope/vim-fugitive')
     use {
@@ -25,78 +25,80 @@ return require('packer').startup(function(use)
         branch = 'v2.x',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
-    }
-}
-use {
-    "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-}
-use("andweeb/presence.nvim")
-use("lukas-reineke/indent-blankline.nvim")
-use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup{
-            ---Add a space b/w comment and the line
-            padding = true,
-            ---Whether the cursor should stay at its position
-            sticky = true,
-            ---Lines to be ignored while (un)comment
-            ignore = nil,
-            ---LHS of toggle mappings in NORMAL mode
-            toggler = {
-                ---Line-comment toggle keymap
-                line = 'gcc',
-                ---Block-comment toggle keymap
-                block = 'gbc',
-            },
-            ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-            opleader = {
-                ---Line-comment keymap
-                line = 'gc',
-                ---Block-comment keymap
-                block = 'gb',
-            },
-            ---LHS of extra mappings
-            extra = {
-                ---Add comment on the line above
-                above = 'gcO',
-                ---Add comment on the line below
-                below = 'gco',
-                ---Add comment at the end of line
-                eol = 'gcA',
-            },
-            ---Enable keybindings
-            ---NOTE: If given `false` then the plugin won't create any mappings
-            mappings = {
-                ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-                basic = true,
-                ---Extra mapping; `gco`, `gcO`, `gcA`
-                extra = true,
-            },
-            ---Function to call before (un)comment
-            pre_hook = nil,
-            ---Function to call after (un)comment
-            post_hook = nil,
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
         }
-    end
-}
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
+    }
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+    use("andweeb/presence.nvim")
+    use("lukas-reineke/indent-blankline.nvim")
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup {
+                ---Add a space b/w comment and the line
+                padding = true,
+                ---Whether the cursor should stay at its position
+                sticky = true,
+                ---Lines to be ignored while (un)comment
+                ignore = nil,
+                ---LHS of toggle mappings in NORMAL mode
+                toggler = {
+                    ---Line-comment toggle keymap
+                    line = 'gcc',
+                    ---Block-comment toggle keymap
+                    block = 'gbc',
+                },
+                ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+                opleader = {
+                    ---Line-comment keymap
+                    line = 'gc',
+                    ---Block-comment keymap
+                    block = 'gb',
+                },
+                ---LHS of extra mappings
+                extra = {
+                    ---Add comment on the line above
+                    above = 'gcO',
+                    ---Add comment on the line below
+                    below = 'gco',
+                    ---Add comment at the end of line
+                    eol = 'gcA',
+                },
+                ---Enable keybindings
+                ---NOTE: If given `false` then the plugin won't create any mappings
+                mappings = {
+                    ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+                    basic = true,
+                    ---Extra mapping; `gco`, `gcO`, `gcA`
+                    extra = true,
+                },
+                ---Function to call before (un)comment
+                pre_hook = nil,
+                ---Function to call after (un)comment
+                post_hook = nil,
+            }
+        end
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use("kristijanhusak/vim-carbon-now-sh")
+    use("tpope/vim-fugitive")
 end)
-
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -75,7 +75,7 @@ time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["Comment.nvim"] = {
-    config = { "\27LJ\2\nç\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\2\nbasic\2\nextra\2\nextra\1\0\3\beol\bgcA\nabove\bgcO\nbelow\bgco\ropleader\1\0\2\nblock\agb\tline\agc\ftoggler\1\0\2\nblock\bgbc\tline\bgcc\1\0\2\fpadding\2\vsticky\2\nsetup\fComment\frequire\0" },
+    config = { "\27LJ\2\nç\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\2\nbasic\2\nextra\2\nextra\1\0\3\beol\bgcA\nbelow\bgco\nabove\bgcO\ropleader\1\0\2\nblock\agb\tline\agc\ftoggler\1\0\2\nblock\bgbc\tline\bgcc\1\0\2\fpadding\2\vsticky\2\nsetup\fComment\frequire\0" },
     loaded = true,
     path = "/Users/aidanboland/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
@@ -178,6 +178,11 @@ _G.packer_plugins = {
     path = "/Users/aidanboland/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["vim-carbon-now-sh"] = {
+    loaded = true,
+    path = "/Users/aidanboland/.local/share/nvim/site/pack/packer/start/vim-carbon-now-sh",
+    url = "https://github.com/kristijanhusak/vim-carbon-now-sh"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/Users/aidanboland/.local/share/nvim/site/pack/packer/start/vim-fugitive",
@@ -186,18 +191,18 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: Comment.nvim
-time([[Config for Comment.nvim]], true)
-try_loadstring("\27LJ\2\nç\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\2\nbasic\2\nextra\2\nextra\1\0\3\beol\bgcA\nabove\bgcO\nbelow\bgco\ropleader\1\0\2\nblock\agb\tline\agc\ftoggler\1\0\2\nblock\bgbc\tline\bgcc\1\0\2\fpadding\2\vsticky\2\nsetup\fComment\frequire\0", "config", "Comment.nvim")
-time([[Config for Comment.nvim]], false)
--- Config for: nvim-autopairs
-time([[Config for nvim-autopairs]], true)
-try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
-time([[Config for nvim-autopairs]], false)
 -- Config for: nvim-jellybeans
 time([[Config for nvim-jellybeans]], true)
 try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\27colorscheme jellybeans\bcmd\bvim\0", "config", "nvim-jellybeans")
 time([[Config for nvim-jellybeans]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
+-- Config for: Comment.nvim
+time([[Config for Comment.nvim]], true)
+try_loadstring("\27LJ\2\nç\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\2\nbasic\2\nextra\2\nextra\1\0\3\beol\bgcA\nbelow\bgco\nabove\bgcO\ropleader\1\0\2\nblock\agb\tline\agc\ftoggler\1\0\2\nblock\bgbc\tline\bgcc\1\0\2\fpadding\2\vsticky\2\nsetup\fComment\frequire\0", "config", "Comment.nvim")
+time([[Config for Comment.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
